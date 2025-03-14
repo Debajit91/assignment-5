f663f30932e9ff1821179c17a6b0a9c77c06bb61
Debajit91 <debajitroy544@gmail.com>
2025-03-14 13:23:30 +0600

    date & day updated

--- scripts/script.js
+++ scripts/script.js
@@ -10,3 +10,16 @@ document.getElementById('themeBtn').addEventListener('click', function(event){
 document.getElementById('discoverNew').addEventListener('click', function(){
     window.location.href = "blogs.html"
 })
+
+function updateDayDate(){
+    const shortDay = new Date().toLocaleString('en-us', {weekday: 'short'})
+    const month = new Date().toLocaleString('en-us', {month: 'short'});
+    const day = new Date().getDate();
+    const year = new Date().getFullYear();
+    const formattedDate = `${month} ${day} ${year}`;
+    document.getElementById('currentDay').firstChild.textContent = `${shortDay} ,`;
+    document.getElementById('currentDate').textContent = formattedDate;
+}
+
+updateDayDate();
+setInterval(updateDayDate, 60000);
